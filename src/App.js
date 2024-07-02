daca29874559e3b6c2f58fbbe0cb4127ed17d63b
KNIGHTW4LKER <samyakaditya0000@gmail.com>
2024-07-02 19:17:00 +0530

    testimonials component

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ function App() {
         
         <HomePage />
         <AboutPage />
+        <TestimonialsPage />
       
         {/* <Container>
           <Routes>
--- src/pages/TestimonialsPage.js
+++ src/pages/TestimonialsPage.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Typography, Container, List, ListItem, ListItemText } from '@mui/material';
+import './TestimonialsPage.css';
 
 const testimonials = [
+  { name: "Shahe | Music Artist", feedback: "Fruit Creatives transformed our online presence with a stunning website that perfectly captures our brand essence." },
   { name: "Client 1", feedback: "FruitCreatives did an amazing job!" },
   { name: "Client 2", feedback: "Highly recommend FruitCreatives for web development." },
   // Add more testimonials here
@@ -9,16 +11,16 @@ const testimonials = [
 
 const TestimonialsPage = () => {
   return (
-    <Container>
-      <Typography variant="h2" component="h1" gutterBottom>
+    <Container maxWidth={'100vw'} className="testimonials-container">
+      <Typography variant="h2" component="h1" gutterBottom className="testimonials-title">
         Client Testimonials
       </Typography>
       <List>
         {testimonials.map((testimonial, index) => (
-          <ListItem key={index}>
+          <ListItem key={index} className="testimonial-item">
             <ListItemText
-              primary={testimonial.name}
-              secondary={testimonial.feedback}
+              primary={<Typography variant="h6" className="testimonial-name">{testimonial.name}</Typography>}
+              secondary={<Typography lineHeight={1.1} maxWidth={'60vw'} fontFamily={ "Noto Serif, serif"} fontSize={70} fontWeight={800} variant="body1" className="testimonial-feedback">{testimonial.feedback}</Typography>}
             />
           </ListItem>
         ))}
