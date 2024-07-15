45ad509097cd74c6168506ba7928aeb701f5c1d7
KNIGHTW4LKER <samyakaditya0000@gmail.com>
2024-07-15 23:57:33 +0530

    contact card component

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,35 +1,39 @@
 import React from 'react';
 import './ContactPage.css';
-import { Typography, Container, TextField, Button, Box } from '@mui/material';
+import { Typography, Container, TextField, Button, Box, Card, CardContent } from '@mui/material';
 
 const ContactPage = () => {
   return (
-    <Container clasName="Contactcontainer" >
-      <Typography variant="h2" component="h1" gutterBottom>
-        Contact Us
-      </Typography>
-      <Box
-        component="form"
-        sx={{
-          '& .MuiTextField-root': { m: 1, width: '25ch' },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField required id="name" label="Name" variant="outlined" />
-        <TextField required id="email" label="Email" variant="outlined" />
-        <TextField
-          required
-          id="message"
-          label="Message"
-          variant="outlined"
-          multiline
-          rows={4}
-        />
-        <Button variant="contained" color="primary" sx={{ m: 1 }}>
-          Send
-        </Button>
-      </Box>
+    <Container maxWidth={'100vw'}  className="Contactcontainer">
+      <Card className="contact-card">
+        <CardContent>
+          <Typography variant="h2" component="h1" gutterBottom className="contact-heading">
+            Contact Us
+          </Typography>
+          <Box
+            component="form"
+            sx={{
+              '& .MuiTextField-root': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField required id="name" label="Name" variant="outlined" />
+            <TextField required id="email" label="Email" variant="outlined" />
+            <TextField
+              required
+              id="message"
+              label="Message"
+              variant="outlined"
+              multiline
+              rows={4}
+            />
+            <Button variant="contained" className="contact-button" sx={{ m: 1 }}>
+              Send
+            </Button>
+          </Box>
+        </CardContent>
+      </Card>
     </Container>
   );
 };
